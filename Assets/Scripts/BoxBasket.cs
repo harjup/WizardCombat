d29d781047fefe19fed81446be499a2b10810bc0
harjup <pikmin@gmail.com>
2015-03-03 21:28:24 -0500

    Destroy box and spawn effect on collision with a goal volume

---
+++ Assets/Scripts/BoxBasket.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxBasket : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        var movableBox = collision.gameObject.GetComponent<MovableBox>();
+        if (movableBox != null)
+        {
+            movableBox.PoofAway();
+        }
+    }
+
+
+}
--- Assets/Scripts/MovableBox.cs
+++ Assets/Scripts/MovableBox.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
+using ModestTree;
 
 public class MovableBox : MonoBehaviourBase
 {
     private Rigidbody _rigidbody;
     private Collider _collider;
     private Transform _initialParent;
-
+    private GameObject _boxConfetti;
 
     public void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _collider = GetComponent<Collider>();
         _initialParent = transform.parent;
+
+        // Forgive me, o great lords of dependency injection
+        // TODO: Do the DI 
+        _boxConfetti = Resources.Load<GameObject>("OrbCollect/BoxBreak");
+        Assert.IsNotNull(_boxConfetti);
     }
 
     public void ResetParent()
@@ -31,5 +38,12 @@ public class MovableBox : MonoBehaviourBase
     {
         _rigidbody.isKinematic = true;
         _collider.enabled = false;
+    }
+
+    public void PoofAway()
+    {
+        transform.DOScale(Vector3.one * .1f, 1f).SetEase(Ease.OutExpo);
+        Instantiate(_boxConfetti, transform.position, Quaternion.identity);
+        Destroy(gameObject, 1.5f);
     }
 }
